b7454a96dfcce318016514075c506b91ad0bd0e1
iwasa-kosui <dev@kosui.me>
2024-03-21 04:35:28 +0900

    initial commit

---
+++ dot_config/zsh/alias.zsh
@@ -0,0 +1,14 @@
+alias c='code'
+alias c.='code .'
+alias d='docker compose'
+alias e='exec $SHELL -l'
+alias o='open'
+alias o.='open .'
+t () {
+	tmux attach -t $1 2> /dev/null || tmux new -s $1 2> /dev/null || tmux ls
+}
+_t() { _values 'sessions' "${(@f)$(tmux ls -F '#S' 2>/dev/null )}" }
+compdef _t t
+alias tmux="TERM=xterm-256color tmux"
+alias x='/Applications/Xcode.app/Contents/MacOS/Xcode'
+alias x.='x .'
