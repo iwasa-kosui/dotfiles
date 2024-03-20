fe54b2713a361cd477a0cf067c12af040d5be44e
iwasa-kosui <dev@kosui.me>
2024-03-21 04:48:33 +0900

    feat: enable comp

--- dot_config/zsh/alias.zsh
+++ dot_config/zsh/alias.zsh
@@ -1,3 +1,11 @@
+autoload -U compinit
+compinit -u
+
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' format '%B%d%b'
+zstyle ':completion:*:warnings' format 'No matches for: %d'
+zstyle ':completion:*' group-name ''
+
 alias c='code'
 alias c.='code .'
 alias d='docker compose'
