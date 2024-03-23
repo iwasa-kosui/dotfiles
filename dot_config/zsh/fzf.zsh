14c81f58c1f356140a85fbffacca541982e53304
iwasa-kosui <dev@kosui.me>
2024-03-23 13:03:45 +0900

    feat: add fzf

---
+++ dot_config/zsh/fzf.zsh
@@ -0,0 +1,10 @@
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
+  
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
