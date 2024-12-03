26089ce9c85c79ed0c521525fa00b7256418b639
Kosui Iwasa <24590869+iwasa-kosui@users.noreply.github.com>
2024-12-03 20:26:19 +0900

    vim

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,25 @@
+if vim.g.vscode then
+  -- VSCode Neovim
+  require "user.vscode_keymaps"
+else
+  -- Ordinary Neovim
+  vim.g.mapleader = ' ' 
+  vim.o.expandtab = true
+  vim.o.tabstop = 2
+  vim.o.shiftwidth = 2
+  vim.api.nvim_set_keymap('n', '<Leader>t', ':NERDTreeFocus<CR>', {noremap=true})
+  vim.api.nvim_set_keymap('n', '<Leader>f', ':NERDTreeFind<CR>', {noremap=true})
+
+  vim.cmd [[packadd packer.nvim]]
+  return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+      'neoclide/coc.nvim',
+      branch = 'release',
+    }
+    use 'Xuyuanp/nerdtree-git-plugin'
+    use 'airblade/vim-gitgutter'
+    use 'preservim/nerdtree'
+  end)
+end
+
