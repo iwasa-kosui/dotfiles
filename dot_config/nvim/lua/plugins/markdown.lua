dcf1b65eb948843489c27c9878e3555a6f06ed71
Kosui Iwasa <24590869+iwasa-kosui@users.noreply.github.com>
2026-07-08 09:11:10 +0900

    chore: diagram.nvimを削除

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -18,19 +18,4 @@ return {
       },
     },
   },
-  {
-    "3rd/diagram.nvim",
-    dependencies = { "3rd/image.nvim" },
-    opts = {
-      events = {
-        render_buffer = { "InsertLeave", "TextChanged", "BufEnter" },
-        clear_buffer = { "BufLeave", "InsertEnter" },
-      },
-      renderer_options = {
-        mermaid = {
-          theme = "default",
-        },
-      },
-    },
-  },
 }
